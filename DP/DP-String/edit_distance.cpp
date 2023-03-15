1fb9d37a3cdda6880ecb7f01de50f0b8d9a6ba09
chavda-mitul <20dce020@charusat.edu.in>
2023-03-15 23:58:54 +0530

    edited edit_distance

--- DP/DP-String/edit_distance.cpp
+++ DP/DP-String/edit_distance.cpp
@@ -1,57 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int f(int i, int j, string s1, string s2)
+int editDistance(int i, int j, string s1, string s2)
 {
-    if (i < 0 || j < 0)
-        return 0;
+    // base case
+    if (i < 0)
+        return j + 1;
+    if (j < 0)
+        return i + 1;
 
     if (s1[i] == s2[j])
     {
-        return 1 + f(i - 1, j - 1, s1, s2);
+        return 0 + editDistance(i - 1, j - 1, s1, s2);
     }
-    else
-    {
-        return 0 + max(f(i - 1, j, s1, s2), f(i, j - 1, s1, s2));
-    }
-}
-
-int lcs(string &s1, string &s2)
-{
-    //	Write your code here.
-
-    int n = s1.size();
-    int m = s2.size();
-
-    vector<int> prev(m + 1, 0), cur(m + 1, 0);
-
-    int ans = 0;
-
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= m; j++)
-        {
-            if (s1[i - 1] == s2[j - 1])
-            {
-                int val = 1 + prev[j - 1];
-                cur[j] = val;
-                ans = max(ans, val);
-            }
-            else
-                cur[j] = 0;
-        }
-        prev = cur;
-    }
-
-    return ans;
+    return min(min(
+                   1 + editDistance(i - 1, j, s1, s2),  // delete
+                   1 + editDistance(i, j - 1, s1, s2)), // insert
+               1 + editDistance(i - 1, j - 1, s1, s2)); // replace
 }
 
 int main()
 {
     string s1 = "intention", s2 = "execution";
-    int n = s1.size();
-    int k = lcs(s1, s2);
-    int ans = n - k;
-    cout << ans << endl;
+    int n = s1.size(), m = s2.size();
+    cout << editDistance(n - 1, m - 1, s1, s2) << endl;
     return 0;
 }
